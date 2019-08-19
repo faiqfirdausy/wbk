76cc2fd748a26c811cc79dda4db9c98eece57c25
vinvin1010 <nurkhabibaratna@gmail.com>
2019-08-19 16:36:03 +0700

    video viewer

--- resources/views/layouts/appfront.blade.php
+++ resources/views/layouts/appfront.blade.php
@@ -125,6 +125,10 @@
 	<a href="javascript:void(0);" id="back-top"><i class="fa fa-angle-up fa-3x"></i></a>
 	<!-- Essential jQuery Plugins
 		================================================== -->
+				
+		<!--Video-->
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+		<script src="https://cdn.rawgit.com/JacobLett/bootstrap4-latest/master/bootstrap-4-latest.min.js"></script>
 		<!-- Main jQuery -->
 		<script src="{{ asset('frontend/js/jquery-1.11.1.min.js')}}"></script>
 		<!-- Single Page Nav -->
@@ -163,5 +167,46 @@
 		</script> 
 		<!-- Custom Functions -->
 		<script src="{{ asset('frontend/js/custom.js')}}"></script>
+		
+		<!--Video-->
+		<script>
+			$(document).ready(function() {
+
+			// Gets the video src from the data-src on each button
+
+			var $videoSrc;  
+			$('.video-btn').click(function() {
+				$videoSrc = $(this).data( "src" );
+			});
+			console.log($videoSrc);
+
+			  
+			  
+			// when the modal is opened autoplay it  
+			$('#myModal').on('shown.bs.modal', function (e) {
+				
+			// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+			$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+			})
+			  
+
+
+			// stop playing the youtube video when I close the modal
+			$('#myModal').on('hide.bs.modal', function (e) {
+				// a poor man's stop video
+				$("#video").attr('src',$videoSrc); 
+			}) 
+				
+				
+
+
+			  
+			  
+			// document ready  
+			});
+
+
+
+		</script>
     </body>
 </html>
