bbbbb8db6d47d9bbef2969e32045db6961df4f24
vinvin1010 <nurkhabibaratna@gmail.com>
2019-09-03 11:37:19 +0700

    no message

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -24,30 +24,25 @@
 			 <div class="col-md-12">
 				 <div class="box box-primary">
             <div class="box-header with-border">
-              <h3 class="box-title">Quick Example</h3>
+              <h3 class="box-title">Unggah Video</h3>
             </div>
             <!-- /.box-header -->
             <!-- form start -->
             <form role="form">
               <div class="box-body">
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
+                  <label for="exampleInputEmail1">Judul Video</label>
                   <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
+                  <label for="exampleInputPassword1">Link Video</label>
                   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputFile">File input</label>
+                  <label for="exampleInputFile">Gambar</label>
                   <input type="file" id="exampleInputFile">
 
-                  <p class="help-block">Example block-level help text here.</p>
-                </div>
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Check me out
-                  </label>
+                  <p class="help-block">Ukuran gambar 400x300</p>
                 </div>
               </div>
               <!-- /.box-body -->
