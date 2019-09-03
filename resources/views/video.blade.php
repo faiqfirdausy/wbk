b8610932e4c963e4cf7c92a0fea5d3bcd980f65a
vinvin1010 <nurkhabibaratna@gmail.com>
2019-09-03 14:36:58 +0700

    no message

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -17,14 +17,14 @@
 	   <!-- Application buttons -->
           <div class="box">
             <div class="box-header">
-              <center><h2 class="box-title">  </h2></center>
+              <h3 class="box-title">Unggah Video</h3>
 			   <center><p></p></center>
             </div>
             <div class="box-body">
 			 <div class="col-md-12">
 				 <div class="box box-primary">
             <div class="box-header with-border">
-              <h3 class="box-title">Unggah Video</h3>
+             
             </div>
             <!-- /.box-header -->
             <!-- form start -->
@@ -32,17 +32,17 @@
               <div class="box-body">
                 <div class="form-group">
                   <label for="exampleInputEmail1">Judul Video</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Judul Video">
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Link Video</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Link Video">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputFile">Gambar</label>
+                  <label for="exampleInputFile">Gambar Cover Video</label>
                   <input type="file" id="exampleInputFile">
 
-                  <p class="help-block">Ukuran gambar 400x300</p>
+                  <p class="help-block">*ukuran dimensi gambar 400x300</p>
                 </div>
               </div>
               <!-- /.box-body -->
