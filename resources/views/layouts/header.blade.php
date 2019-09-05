1ebd423059a5f308fdec3c63fc7478abf9af1e5a
vinvin1010 <nurkhabibaratna@gmail.com>
2019-09-05 15:34:45 +0700

    no message

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -109,9 +109,10 @@
           </ul>
 
         </li>
-        <li ><a href="{{url('video')}}"><i class="fa fa-laptop"></i>Video</a></li>
-		<li ><a href="{{url('ipkikm')}}"><i class="fa fa-laptop"></i>IPK/IKM</a></li>
-		<li ><a href="{{url('acplan')}}"><i class="fa fa-laptop"></i>Action Plan</a></li>
+       
+		<li ><a href="{{url('ipkikm')}}"><i class="fa fa-bar-chart"></i>IPK/IKM</a></li>
+		<li ><a href="{{url('acplan')}}"><i class="fa fa-pencil-square-o"></i>Action Plan</a></li>
+		 <li ><a href="{{url('video')}}"><i class="fa fa-camera"></i>Video</a></li>
 
         
     </section>
